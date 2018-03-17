4b513f9708e8d998ee49f8f938acf0edb63488af
TaufanFadhilah <tfi231097@gmail.com>
2018-03-18 01:46:09 +0700

    adding progress bar

--- app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul4/ListName.java
+++ app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul4/ListName.java
@@ -1,5 +1,6 @@
 package com.example.android.taufan_fadhilah_iskandar_1202150017_modul4;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 public class ListName extends AppCompatActivity {
 
     ListView lvName;
+    ProgressDialog mProgressBar;
     private String[] names = {
             "Taufan", "Fadhilah", "Iskandar", "Hanan", "Attaki"
     };
@@ -26,12 +29,20 @@ public class ListName extends AppCompatActivity {
     }
 
     public void startAsync(View view) {
+        mProgressBar = new ProgressDialog(ListName.this);
+        mProgressBar.setCancelable(true);
+        mProgressBar.setTitle("Loading Data");
+        mProgressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressBar.setMax(100);
+        mProgressBar.setProgress(0);
+        mProgressBar.show();
         new myTask().execute();
     }
 
     class myTask extends AsyncTask<Void, String, Void>{
 
         private ArrayAdapter<String> adapter;
+        int count = 0;
         @Override
         protected void onPreExecute() {
             adapter=(ArrayAdapter<String>)lvName.getAdapter();
@@ -40,7 +51,14 @@ public class ListName extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
             for (String item: names){
-                publishProgress(item);
+                try {
+                    publishProgress(item);
+                    count += 20;
+                    Thread.sleep(1000); // 2 segundos
+
+                } catch(InterruptedException e) {
+
+                }
             }
             return null;
         }
@@ -48,10 +66,14 @@ public class ListName extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(String... values) {
             adapter.add(values[0]);
+            int progress = count;
+            mProgressBar.setProgress(progress);
+            mProgressBar.setMessage(values[0]+" ("+progress+"%)");
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
+            mProgressBar.cancel();
             Toast.makeText(lvName.getContext(),"Async Complete",Toast.LENGTH_SHORT).show();
         }
     }
