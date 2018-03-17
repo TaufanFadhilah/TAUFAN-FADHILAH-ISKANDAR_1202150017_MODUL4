20ff1624e6747bc45d60895af6cfc2e6e445af22
TaufanFadhilah <tfi231097@gmail.com>
2018-03-17 23:57:11 +0700

    init project

---
+++ app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.taufan_fadhilah_iskandar_1202150017_modul4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
