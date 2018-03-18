0c212d46b5d5d376cc526b62a8e86e73c02852d5
TaufanFadhilah <tfi231097@gmail.com>
2018-03-18 16:31:37 +0700

    onSaveInstanceState done

--- app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul4/ListName.java
+++ app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul4/ListName.java
@@ -4,11 +4,9 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
